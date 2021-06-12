090784538bef5dcc0c8ba5d36761de33e515a563
Konstantin Kuksov <kkuksov@gmail.com>
2021-06-12 13:49:05 +0300

    Feat: create server & db connection
    Create express server, create and test database connection, create .env file with environment variables

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express';
+import { sequelize } from './db';
+
+const app: Application = express();
+const PORT = process.env.PORT || 5500;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello!');
+});
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
